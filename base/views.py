e89352dc62762ee067cb55b44e2982efb0fbe7d6
artdidit2506 <artdidit2506@gmail.com>
2022-06-06 10:02:07 +0700

    update file base dan templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'base/index.html')
+
+def users(request):
+    return render(request, 'base/users-profile.html')
+
+def pages(request):
+    return render(request, 'base/pages-faq.html')
