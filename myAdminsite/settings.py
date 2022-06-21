7db00b081a47b173f7de8d62416f5f9158756790
artdidit2506 <artdidit2506@gmail.com>
2022-06-21 22:22:27 +0700

    update waktu

--- myAdminsite/settings.py
+++ myAdminsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myAdminsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adminPos1',
+        'USER': 'root',
+        'PASSWORD': '!200909alexaA#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
